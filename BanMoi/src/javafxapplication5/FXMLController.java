687ea1c49aa1b97daa970eb43d68df8e8e25618e
Dqlinh0802 <dqlinh0802@gmail.com>
2020-11-15 15:49:23 +0700

    Ban moi

---
+++ BanMoi/src/javafxapplication5/FXMLController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication5;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Acer
+ */
+public class FXMLController implements Initializable {
+
+    
+    public void Thoat(ActionEvent event){
+       System.exit(0);
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
