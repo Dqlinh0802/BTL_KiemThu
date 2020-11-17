67512044447579dcc7bf2edaf44409443c740c23
Dqlinh0802 <73267666+Dqlinh0802@users.noreply.github.com>
2020-11-17 11:03:01 +0700

    Add files via upload
    Chinh sua giao dien

--- BanMoi/src/javafxapplication5/Main.java
+++ BanMoi/src/javafxapplication5/Main.java
@@ -9,7 +9,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,12 +22,15 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("TrangChu.fxml"));
-
+        stage.initStyle(StageStyle.TRANSPARENT);
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
+        
+        
     }
-
+    
     public static void main(String[] args) {
         launch(args);
     }
