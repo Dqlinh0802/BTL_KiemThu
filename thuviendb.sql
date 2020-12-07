eb95bed53bed572c11292db225aeca82a3287c56
Dqlinh0802 <dqlinh0802@gmail.com>
2020-12-07 21:57:52 +0700

    csdl

---
+++ thuviendb.sql
@@ -0,0 +1,102 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: thuviendb
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `loaisach`
+--
+
+DROP TABLE IF EXISTS `loaisach`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loaisach` (
+  `maLoaiSach` int NOT NULL,
+  `tenLoaiSach` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`maLoaiSach`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loaisach`
+--
+
+LOCK TABLES `loaisach` WRITE;
+/*!40000 ALTER TABLE `loaisach` DISABLE KEYS */;
+INSERT INTO `loaisach` VALUES (4,'Truyện'),(6,'Tiểu thuyết'),(32,'Kinh dị');
+/*!40000 ALTER TABLE `loaisach` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sach`
+--
+
+DROP TABLE IF EXISTS `sach`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sach` (
+  `maSach` char(10) NOT NULL,
+  `tenSach` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `soLuong` int NOT NULL,
+  `namXuatBan` varchar(50) NOT NULL,
+  `moTaSach` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`maSach`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sach`
+--
+
+LOCK TABLES `sach` WRITE;
+/*!40000 ALTER TABLE `sach` DISABLE KEYS */;
+INSERT INTO `sach` VALUES ('CSDL01','Cơ sở dữ liệu',2,'2000','Lập trình cơ sở dữ liệu'),('CSLT01','Cơ sở lập trình',25,'2018','Nhập môn lập trình'),('DSTT01','Đại số tuyến tính',12,'2002','Toán'),('HDT01','Hướng đối tượng',3,'1995','Lập trình '),('KTLT01','Kĩ thuật lập trình',42,'1998','Lập trình nâng cao'),('MMT01','Mạng máy tính',34,'2009','Mạng'),('PLDC01','Pháp luật đại cương',12,'2009','Pháp luật '),('TA01','Tiếng anh nâng cao 1',22,'2006','Tiếng anh'),('TA02','Tiếng anh nâng cao 2',21,'2009','Tiếng anh'),('TO01','Toán cao cấp A1',12,'2015','Toán '),('TO02','Toán cao cấp A2',34,'1998','Toán'),('XSTK01','Xác suất thống kê',67,'2004','Xác suất');
+/*!40000 ALTER TABLE `sach` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tacgia`
+--
+
+DROP TABLE IF EXISTS `tacgia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tacgia` (
+  `maTacGia` int NOT NULL,
+  `tenTacGia` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`maTacGia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tacgia`
+--
+
+LOCK TABLES `tacgia` WRITE;
+/*!40000 ALTER TABLE `tacgia` DISABLE KEYS */;
+INSERT INTO `tacgia` VALUES (2,'Địa Địa'),(3,'Quang Địa');
+/*!40000 ALTER TABLE `tacgia` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-07 21:56:40
